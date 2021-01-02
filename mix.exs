9e0d345b707fc5f3a0d10d05b79fa6bd3a664856
Tim Pile <timspile@gmail.com>
2021-01-02 11:54:53 -0500

    Add components to page

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule PhoenixFeathersDemo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_feathers, "~> 0.2.1-alpha"}
+      {:phoenix_feathers, "~> 0.3.0-alpha"}
     ]
   end
 
