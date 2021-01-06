523712c5b7f364b70a511cc25ff4fc74be7c5dc3
Tim Pile <timspile@gmail.com>
2021-01-06 11:41:26 -0500

    Add tool tips section

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule PhoenixFeathersDemo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_feathers, "~> 1.0.1-alpha"}
+      {:phoenix_feathers, "~> 1.1.0-alpha"}
     ]
   end
 
